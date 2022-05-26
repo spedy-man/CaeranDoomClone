252c1f5df6220c175b7b0d1673d36a0a618bdd67
spedy-man <88007938+spedy-man@users.noreply.github.com>
2022-05-27 09:57:56 +1200

    finished character movement, added pistol sprites and mouse movement

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -23,13 +23,22 @@ func get_input():
 		input_dir += -global_transform.basis.x
 	if Input.is_action_pressed("strafe_right"):
 		input_dir += global_transform.basis.x
+	#input_dir  = input_dir.normalized(), this command cancels strafing
+	return input_dir
 
 func _unhandled_input(event):
-	pass
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * mouse_senstivity)
+		$Pivot.rotate_x(-event.relative.y * mouse_senstivity)
+		$Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2,1.2)
 
 func _physics_process(delta):
 	#gravity
-	velocity += gravity * delta
+	velocity.y += gravity * delta
+	var desired_velocity = get_input() * max_speed
+	velocity.x = desired_velocity.x
+	velocity.z = desired_velocity.z
+	velocity = move_and_slide(velocity, Vector3.UP, true)
 
 func change_gun():
 	pass
