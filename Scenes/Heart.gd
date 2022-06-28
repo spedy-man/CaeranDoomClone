f85dbfc48ff92abba10da1af5a9fb483a9bed31f
spedy-man <88007938+spedy-man@users.noreply.github.com>
2022-06-28 12:26:16 +1200

    finished rocket, editing to do

--- Scenes/Heart.gd
+++ Scenes/Heart.gd
@@ -4,9 +4,12 @@ var can_shoot = true
 onready var gunsprite = $CanvasLayer/Control/GunSprite
 onready var spawn_location = $Position3D
 onready var explosion
+onready var blood = preload("res://Scenes/explosion.tscn")
 
 func launch_projectile():
-	pass
+	var new_blood = blood.instance()
+	get_node("/root/World").add_child(new_blood)
+	new_blood.global_transform = spawn_location.global_transform
 
 func _process(delta):
 	if Input.is_action_pressed("shoot") and can_shoot:
--- Scenes/explosion.gd
+++ Scenes/explosion.gd
@@ -1,23 +1,29 @@
 extends Area
 
-var blood_speed = 10
+var blood_speed = 15
+var blood_damage = 25
 
-func deal_damage(amount):
+func deal_damage():
 	var enemies = get_overlapping_bodies()
 	for body in enemies:
 		if body.is_in_group("Enemy"):
-			body.take_damage(projectile_damage)
+			body.take_damage(blood_damage)
 	enemies = $SplashDamage.get_overlapping_bodies()
 	for body in enemies:
 		if body.is_in_group("Enemy"):
-			body.take_damage(projectile_splash)
+			body.take_damage(blood_damage)
 
 func _process(delta):
-	translate(Vector3.FORWARD * speed * delta)
+	translate(Vector3.FORWARD * blood_speed * delta)
 
 func _on_explosion_body_entered(body):
-	pass
-
+	if body.is_in_group("Player"):
+		return
+	set_process(false)
+	$AnimatedSprite3D.play("explode")
+	deal_damage()
+	yield($AnimatedSprite3D,"animation_finished")
+	queue_free()
 
 func _on_SplashDamage_body_entered(body):
 	pass
